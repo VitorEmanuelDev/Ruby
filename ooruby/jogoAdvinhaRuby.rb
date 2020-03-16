78c58bb0b138e2e264e986bed2b5d25f21b6e282
Vitor Emanuel <veslima3@gmail.com>
2020-03-16 18:51:55 -0300

    Update jogoAdvinhaRuby.rb

--- ooruby/jogoAdvinhaRuby.rb
+++ ooruby/jogoAdvinhaRuby.rb
@@ -8,33 +8,48 @@ end
 
 def sorteia_mumero_secreto
     puts "Escolhendo um número secreto entre 0 e 200..."
-    numero_secreto = 222
+    sorteado = 222
     puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
+    sorteado
 end
 
-da_boas_vindas
-sorteia_numero_secreto
-
-limite_de_tentativas = 5
-
-for tentativa in 1..limite_de_tentativas
+def pede_um_numero(tentativa, limite_de_tentativas)
     puts "\n\n\n"
     puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas
     puts "Entre com o numero"
     chute = gets
-    puts "Será que acertou? Você chutou " + chute 
-    
+    puts "Será que acertou? Você chutou " + chute
+    chute.to_i
+end
+
+def verifica_se_acertou
     acertou = numero_secreto == chute.to_i
 
     if acertou
         puts "Acertou"
-        break
+        return true
     else
         maior = numero_secreto > chute.to_i
         if maior
             puts "O número secreto é maior."
+            return false
         else
             puts "O número secreto é menor."
+            return false
         end
+    end
+end
+
+
+da_boas_vindas
+numero_secreto = sorteia_numero_secreto
+
+limite_de_tentativas = 5
+
+for tentativa in 1..limite_de_tentativas
+     
+    chute = pede_um_numero(tentativa, limite_de_tentativas)
+    if verifica_se_acertou
+        break
     end
 end
