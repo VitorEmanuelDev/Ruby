61ebe5b25d5a8ba126cb8dd848ae995a7ddf3f83
Vitor Emanuel <veslima3@gmail.com>
2020-03-16 23:26:08 -0300

    Update jogoAdvinhaRuby.rb

--- ooruby/jogoAdvinhaRuby.rb
+++ ooruby/jogoAdvinhaRuby.rb
@@ -1,43 +1,40 @@
 def da_boas_vindas
-    puts "Bem vindo ao jogo da adivinhação"
+    puts "Bem-vindo ao jogo da adivinhação"
     puts "Qual é o seu nome?"
     nome = gets
-    puts "\n\n\n"
+    puts "\n\n\n\n"
     puts "Começaremos o jogo para você, " + nome
 end
 
-def sorteia_mumero_secreto
+def sorteia_numero_secreto
     puts "Escolhendo um número secreto entre 0 e 200..."
-    sorteado = 222
+    sorteado = 111
     puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
     sorteado
 end
 
 def pede_um_numero(tentativa, limite_de_tentativas)
-    puts "\n\n\n"
-    puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas
-    puts "Entre com o numero"
+    puts "\n\n\n\n"
+    puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
+    puts "Entre com o número"
     chute = gets
-    puts "Será que acertou? Você chutou " + chute
+    puts "Será que acertou? Você chutou #{chute}"
     chute.to_i
 end
 
-def verifica_se_acertou
-    acertou = numero_secreto == chute.to_i
-
+def verifica_se_acertou(numero_secreto, chute)
+    acertou = numero_secreto == chute
     if acertou
-        puts "Acertou"
+        puts "Acertou!"
         return true
-    else
-        maior = numero_secreto > chute.to_i
-        if maior
-            puts "O número secreto é maior."
-            return false
-        else
-            puts "O número secreto é menor."
-            return false
-        end
     end
+    maior = numero_secreto > chute
+    if maior
+        puts "O número secreto é maior!"
+    else
+        puts "O número secreto é menor!"
+    end
+    false
 end
 
 
@@ -47,9 +44,8 @@ numero_secreto = sorteia_numero_secreto
 limite_de_tentativas = 5
 
 for tentativa in 1..limite_de_tentativas
-     
     chute = pede_um_numero(tentativa, limite_de_tentativas)
-    if verifica_se_acertou
+    if verifica_se_acertou(numero_secreto, chute)
         break
     end
 end
